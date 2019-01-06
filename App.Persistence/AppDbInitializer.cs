837efdb430e9178321e34bafd775aab197175f98
Samuel Mwangi <smwangi007@emt.ema.ad.pwcinternal.com>
2019-01-06 03:56:05 +0300

    Update DB Seeding

--- App.Persistence/AppDbInitializer.cs
+++ App.Persistence/AppDbInitializer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Linq;
 
 namespace App.Persistence
 {
@@ -11,18 +12,28 @@ namespace App.Persistence
         {
             var initializer = new AppDbInitializer();
             context.Database.EnsureCreated();
-            initializer.CreateDBViews(context);
+             
+            //For MSSQLServer 
+            initializer.SeedDBUsingSQLScripts(context, GetSQLScriptDirectoryFullPath("DBViews"));
+            
+            // If Table has records do not seed
+            if (!context.Notifications.Any())
+                initializer.SeedDBUsingSQLScripts(context, GetSQLScriptDirectoryFullPath("SeedDBTables"));
         }
 
-        private void CreateDBViews(AppDbContext context)
+        private static string GetSQLScriptDirectoryFullPath(string targetFolder)
         {
-            var targetDirectory = Directory.GetCurrentDirectory() + string.Format("{0}..{0}App.Persistence{0}DBViews", Path.DirectorySeparatorChar);
+            return Directory.GetCurrentDirectory() + string.Format("{0}..{0}App.Persistence{0}DBScripts{0}{1}", Path.DirectorySeparatorChar, targetFolder);
+        }
 
+        private void SeedDBUsingSQLScripts(AppDbContext context, string targetDirectory)
+        {
             string[] fileEntries = Directory.GetFiles(targetDirectory);
             foreach (string fileName in fileEntries)
-            {                
+            {
                 context.Database.ExecuteSqlCommand(File.ReadAllText(fileName));
             }
         }
+
     }
 }
---
+++ App.Persistence/DBScripts/SeedDBTables/SeedNotification.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO dbo.Notifications
+
+VALUES('test.from@example.com','test.to1@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes',1,GETUTCDATE()),
+      ('test.from@example.com','test.to2@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes',1,GETUTCDATE())
+
