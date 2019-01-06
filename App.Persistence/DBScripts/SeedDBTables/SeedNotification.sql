be2fccb8349c0f198f055fae61ab63a8996f820c
Samuel Mwangi <smwangi007@emt.ema.ad.pwcinternal.com>
2019-01-06 04:08:59 +0300

    Update DB Seeding

--- App.Persistence/DBScripts/SeedDBTables/SeedNotification.sql
+++ App.Persistence/DBScripts/SeedDBTables/SeedNotification.sql
@@ -1,6 +1,7 @@
 
 INSERT INTO dbo.Notifications
 
-VALUES('test.from@example.com','test.to1@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes',1,GETUTCDATE()),
-      ('test.from@example.com','test.to2@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes',1,GETUTCDATE())
+VALUES('test.from@example.com', 'test.to1@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes', 1, GETUTCDATE()),
+      ('test.from@example.com', 'test.to2@example.com', 'Sending Good Vibes Yo', 'Receive Good Vibes', 1, GETUTCDATE()),
+      ('test.from1@example.com', 'test.to3@example.com', 'Please pay your Items', 'Sending Reminder', 1, GETUTCDATE())
 
