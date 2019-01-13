5e926a9180ec6f2e5e903ded3e42548d604816b8
Sammie-Mwangi <sammie-mwangi4@gmail.com>
2019-01-14 00:40:12 +0300

    Use Expression Projections EF Core

--- BackEnd/App.Application/EntitiesCommandsQueries/ProductCategories/Queries/GetProductCategories/ProductCategoriesViewModel.cs
+++ BackEnd/App.Application/EntitiesCommandsQueries/ProductCategories/Queries/GetProductCategories/ProductCategoriesViewModel.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 
 namespace App.Application.EntitiesCommandsQueries.ProductCategories.Queries.GetProductCategories
 {
-    public class ProductCategoriesViewModel : ProductCategoryDTO
+    public class ProductCategoriesViewModel 
     {
         public IEnumerable<ProductCategoryDTO> ProductCategories { get; set; }        
         public bool CreateEnabled { get; set; }
-
-
 
     }
 }
