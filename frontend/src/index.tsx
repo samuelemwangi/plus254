208f47b6705c9b5b0b11a18ea725db5383115d95
Samuel Mwangi Irungu <mwangi.s.irungu@gmail.com>
2021-06-06 02:27:58 +0300

    remove redundant packages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import "react-perfect-scrollbar/dist/css/styles.css";
-import "nprogress/nprogress.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -29,4 +28,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// eslint-disable-next-line no-console
+reportWebVitals(console.log);
---
+++ frontend/src/views/Explore/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import type { FC } from "react";
+
+// Custom components
+import Page from "../../components/Page";
+
+// interfaces
+
+const Explore: FC = () => {
+  return <Page title="explore">Hello</Page>;
+};
+
+export default Explore;
